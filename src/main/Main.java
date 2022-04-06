f6e250783d3a6541c32d00e2c02409197399b83a
Rian Furtado <rianoliveirafurtado@gmail.com>
2022-04-05 21:38:56 -0300

    Movimentação WASD

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,23 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Rian-Furtado
-		
-		
-		
+
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
 		window.setTitle("2D Adventure");
-		
+
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
-		
+
 		window.pack();
 
-		
-		window.setLocationRelativeTo(null); 
+		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		
+		gamePanel.startGameThread(); 
+
 	}
 
 }
